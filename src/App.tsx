330b522a4d625d632c95c64c7aae798637f03e46
Kushal <jainkushal785@gmail.com>
2025-06-27 17:41:52 +0530

    mapscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import EnterCodeScreen from './screens/student/BeginScreen'; // relative path, no alias
-
+import OptimizedCosmicMap from './screens/student/LevelMapScreen';
 function App() {
   return (
     <main className="min-h-screen bg-background text-foreground">
-      <EnterCodeScreen />
+      {/* <EnterCodeScreen /> */}
+      <OptimizedCosmicMap />
     </main>
   )
 }

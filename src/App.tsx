06e119f0ef013515a40209c56b4466d8ab6bbab7
Kushal <jainkushal785@gmail.com>
2025-06-27 19:41:44 +0530

    set navigation GITU BOY

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import EnterCodeScreen from './screens/student/BeginScreen'; // relative path, no alias
 import OptimizedCosmicMap from './screens/student/LevelMapScreen';
+import QuestDetailScreen from './screens/student/QuestDetailScreen'
 function App() {
   return (
     <main className="min-h-screen bg-background text-foreground">
       {/* <EnterCodeScreen /> */}
-      <OptimizedCosmicMap />
+      {/* <OptimizedCosmicMap /> */}
+      <QuestDetailScreen />
     </main>
   )
 }
